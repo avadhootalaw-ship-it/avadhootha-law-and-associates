95268c81ea46a07a6bbe87ed444a40da10b8e30c
avadhootalaw-ship-it <avadhootalaw@gmail.com>
2026-09-17 09:36:47 +0530

    Fix Book Appointment forms to work like Contact Us

--- update_appointment_btn.py
+++ update_appointment_btn.py
@@ -1,19 +1,100 @@
-import glob
 import re
 
-files = glob.glob('d:/Projects/avadhootha law and associates/*.html')
-files = [f for f in files if 'admin.html' not in f]
+file_path = r'd:\Projects\avadhootha law and associates\appointment.html'
 
-for file in files:
-    with open(file, 'r', encoding='utf-8', errors='surrogateescape') as f:
-        html = f.read()
+with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+    content = f.read()
 
-    # Replace the Appointment button link in the navbar
-    html = re.sub(
-        r'<a href="contact\.html" class="btn btn-primary py-2 px-4 d-none d-lg-block">Appointment</a>',
-        r'<a href="https://wa.me/919448954564?text=Hello%20Avadhoota%20Law%20Associates%2C%20I%20would%20like%20to%20schedule%20an%20appointment." target="_blank" class="btn btn-primary py-2 px-4 d-none d-lg-block">Appointment</a>',
-        html
-    )
+target = """                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="row g-3">
+                        <div class="col-12 col-sm-6">
+                            <input type="text" class="form-control" placeholder="Your Name" style="height: 55px;">
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <input type="email" class="form-control" placeholder="Your Email" style="height: 55px;">
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <input type="text" class="form-control" placeholder="Your Mobile" style="height: 55px;">
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <select class="form-select" style="height: 55px;">
+                                <option selected>Choose Service</option>
+                                <option value="1">Service 1</option>
+                                <option value="2">Service 2</option>
+                                <option value="3">Service 3</option>
+                            </select>
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <div class="date" id="date" data-target-input="nearest">
+                                <input type="text"
+                                    class="form-control datetimepicker-input"
+                                    placeholder="Choose Date" data-target="#date" data-toggle="datetimepicker" style="height: 55px;">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-6">
+                            <div class="time" id="time" data-target-input="nearest">
+                                <input type="text"
+                                    class="form-control datetimepicker-input"
+                                    placeholder="Choose Date" data-target="#time" data-toggle="datetimepicker" style="height: 55px;">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <textarea class="form-control" rows="5" placeholder="Message"></textarea>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-primary w-100 py-3" type="submit">Book Appointment</button>
+                        </div>
+                    </div>
+                </div>"""
 
-    with open(file, 'w', encoding='utf-8', errors='surrogateescape') as f:
-        f.write(html)
+replacement = """                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <form action="https://formsubmit.co/avadhootalaw@gmail.com" method="POST">
+                        <div class="row g-3">
+                            <div class="col-12 col-sm-6">
+                                <input type="text" class="form-control" name="Name" placeholder="Your Name" style="height: 55px;" required>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <input type="email" class="form-control" name="Email" placeholder="Your Email" style="height: 55px;" required>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <input type="text" class="form-control" name="Mobile" placeholder="Your Mobile" style="height: 55px;" required>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <select class="form-select" name="Service" style="height: 55px;">
+                                    <option selected>Choose Service</option>
+                                    <option value="Service 1">Service 1</option>
+                                    <option value="Service 2">Service 2</option>
+                                    <option value="Service 3">Service 3</option>
+                                </select>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="date" id="date" data-target-input="nearest">
+                                    <input type="text"
+                                        class="form-control datetimepicker-input"
+                                        name="Date" placeholder="Choose Date" data-target="#date" data-toggle="datetimepicker" style="height: 55px;">
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <div class="time" id="time" data-target-input="nearest">
+                                    <input type="text"
+                                        class="form-control datetimepicker-input"
+                                        name="Time" placeholder="Choose Time" data-target="#time" data-toggle="datetimepicker" style="height: 55px;">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <textarea class="form-control" name="Message" rows="5" placeholder="Message"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100 py-3" type="submit">Book Appointment</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>"""
+
+if target in content:
+    content = content.replace(target, replacement)
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(content)
+    print("Success")
+else:
+    print("Target not found. Let's try regex or something else.")
